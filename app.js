3a8d937a9df86af3c147b550083a2bbf462e4ac8
yogeshprithiviraj <2136981@cognizant.com>
2023-05-11 10:50:32 +0530

    more changes

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var http = require('http');
-const url=require('url')
+const url = require('url')
 const express = require('express');
 const pool = require("./dbconnection")
 const bodyParser = require('body-parser');
@@ -9,6 +9,8 @@ const { nextTick } = require('process');
 const app = express();
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+
+
 
 // app.use((req,res)=>{
 //     res.json({ message: 'Hey! This is your server response!' });
@@ -23,41 +25,41 @@ app.use(bodyParser.urlencoded({ extended: true }))
 //         }
 //         response.status(200).json(results.rows)
 //       })
-   
+
 //   });
-  // TODO:query string- localhost:3000/restranet/1
+// TODO:query string- localhost:3000/restranet/1
 
-  app.get("/restaurant/:res_id", (request, response) => {
-    // TODO:
-    const { res_id } = request.params;
-    console.log(res_id);
-   
-    pool.query('SELECT * FROM gr.restaurant where res_id= $1',[res_id] ,(error, results) => {
-        if (error) {
-          throw error
-        }
-        response.status(200).json(results.rows)//
-      })
-     
-  });
+app.get("/restaurant/:res_id", (request, response) => {
+  // TODO:
+  const { res_id } = request.params;
+  console.log(res_id);
 
-  // TODO:url param - localhost:3000/restranet?id=1
-  app.get("/restaurant", (request, response) => {
-    // TODO:
-    const { res_id } = request.query;
-    //console.log("res_id",res_id)
-    pool.query('SELECT * FROM gr.restaurant where res_id= $1',[res_id] ,(error, results) => {
-        if (error) {
-          throw error
-        }
-        response.status(200).json(results.rows)
-      })
-    // response.status(200).json({})
-  });
-  
-  app.post("/restaurant",(request,response)=>{
-    const {res } = request.body
-   pool.query(
+  pool.query('SELECT * FROM gr.restaurant where res_id= $1', [res_id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)//
+  })
+
+});
+
+// TODO:url param - localhost:3000/restranet?id=1
+app.get("/restaurant", (request, response) => {
+  // TODO:
+  const { res_id } = request.query;
+  //console.log("res_id",res_id)
+  pool.query('SELECT * FROM gr.restaurant where res_id= $1', [res_id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+  // response.status(200).json({})
+});
+
+app.post("/restaurant", (request, response) => {
+  const { res } = request.body
+  pool.query(
     'INSERT INTO gr.restaurant(res_name) VALUES ($1)',
     [res],
     (error) => {
@@ -67,11 +69,11 @@ app.use(bodyParser.urlencoded({ extended: true }))
       response.status(201).json({ status: 'success', message: 'restarent added.' })
     }
   )
-  })
+})
 
-  app.delete("/restaurant",(request,response)=>{
-    const {res_name} = request.body
-    pool.query(
+app.delete("/restaurant", (request, response) => {
+  const { res_name } = request.body
+  pool.query(
     'delete from gr.restaurant where res_name=$1',
     [res_name],
     (error) => {
@@ -81,12 +83,12 @@ app.use(bodyParser.urlencoded({ extended: true }))
       response.status(201).json({ status: 'success', message: 'restarent deleted.' })
     }
   )
-  })
-  app.put("/restaurant",(request,response)=>{
-    const {res_id,res_name} = request.body
+})
+app.put("/restaurant", (request, response) => {
+  const { res_id, res_name } = request.body
   pool.query(
     'UPDATE gr.restaurant set res_name=$2 where res_id= $1',
-    [res_id,res_name],
+    [res_id, res_name],
     (error) => {
       if (error) {
         throw error
@@ -94,41 +96,41 @@ app.use(bodyParser.urlencoded({ extended: true }))
       response.status(201).json({ status: 'success', message: 'restarent update' })
     }
   )
+})
+
+
+//-------main menu---------------
+//url params
+app.get("/restaurant/main_menu", (request, response) => {
+  // TODO:
+  const { id } = request.query;
+  pool.query('select * from gr.main_menu where res_id=$1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
   })
 
-
-  //-------main menu---------------
-  //url params
-  app.get("/restaurant/main_menu", (request, response) => {
-    // TODO:
-    const { id } = request.query;
-    pool.query('select * from gr.main_menu where res_id=$1',[id] ,(error, results) => {
-        if (error) {
-          throw error
-        }
-        response.status(200).json(results.rows)
-      })
-   
-  });
+});
 //query string 
-  app.get("/restaurant/main_menu/:res_id", (request, response) => {
-    // TODO:
-    const { res_id } = request.params;
-    pool.query('SELECT * FROM gr.main_menu where res_id=$1',[res_id] ,(error, results) => {
-        if (error) {
-          throw error
-        }
-        response.status(200).json(results.rows)
-      })
-   
-  });
+app.get("/restaurant/main_menu/:res_id", (request, response) => {
+  // TODO:
+  const { res_id } = request.params;
+  pool.query('SELECT * FROM gr.main_menu where res_id=$1', [res_id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
 
-  app.post("/restaurant/main_menu",(request,response)=>{
-    const {menu } = request.body;
-    const{res}=request.body;
-   pool.query(
+});
+
+app.post("/restaurant/main_menu", (request, response) => {
+  const { menu } = request.body;
+  const { res } = request.body;
+  pool.query(
     'INSERT INTO gr.main_menu(menu_name,res_id) VALUES ($1,$2)',
-    [menu,res],
+    [menu, res],
     (error) => {
       if (error) {
         throw error
@@ -136,11 +138,11 @@ app.use(bodyParser.urlencoded({ extended: true }))
       response.status(201).json({ status: 'success', message: 'main menu added with related restarant' })
     }
   )
-  })
+})
 
-  app.delete("/restaurant/main_menu/:id",(request,response)=>{
-    const {id} = request.params
-    pool.query(
+app.delete("/restaurant/main_menu/:id", (request, response) => {
+  const { id } = request.params
+  pool.query(
     'delete from gr.main_menu where mainmenu_id=$1',
     [id],
     (error) => {
@@ -150,12 +152,12 @@ app.use(bodyParser.urlencoded({ extended: true }))
       response.status(201).json({ status: 'success', message: 'menu was deleted' })
     }
   )
-  })
-  app.put("/restaurant/main_menu",(request,response)=>{
-    const {id,name} = request.body
+})
+app.put("/restaurant/main_menu", (request, response) => {
+  const { id, name } = request.body
   pool.query(
     'UPDATE gr.main_menu set menu_name=$2 where mainmenu_id= $1',
-    [id,name],
+    [id, name],
     (error) => {
       if (error) {
         throw error
@@ -163,26 +165,26 @@ app.use(bodyParser.urlencoded({ extended: true }))
       response.status(201).json({ status: 'success', message: 'menu update successfully' })
     }
   )
-  })
+})
 //------------menu catagory---------------------
-  app.get("/restaurant/main_menu/menu_category/:id", (request, response) => {
-    // TODO:
-    const { id } = request.params;
-    pool.query('select * from gr.menu_category where mainmenu_id=$1',[id] ,(error, results) => {
-        if (error) {
-          throw error
-        }
-        response.status(200).json(results.rows)
-      })
-   
-  });
+app.get("/restaurant/main_menu/menu_category/:id", (request, response) => {
+  // TODO:
+  const { id } = request.params;
+  pool.query('select * from gr.menu_category where mainmenu_id=$1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
 
-  app.post("/restaurant/main_menu/menu_category/",(request,response)=>{
-    const {menu } = request.body;
-    const{cat}=request.body;
-   pool.query(
+});
+
+app.post("/restaurant/main_menu/menu_category/", (request, response) => {
+  const { menu } = request.body;
+  const { cat } = request.body;
+  pool.query(
     'INSERT INTO gr.menu_category(name,mainmenu_id) VALUES ($2,$1)',
-    [menu,cat],
+    [menu, cat],
     (error) => {
       if (error) {
         throw error
@@ -190,13 +192,13 @@ app.use(bodyParser.urlencoded({ extended: true }))
       response.status(201).json({ status: 'success', message: 'new menu category added to the menu ' })
     }
   )
-  })
+})
 //TODO
-  app.delete("/restaurant/main_menu/menu_category/:id",(request,response)=>{
-    const {id,fk} = request.params
-    pool.query(
+app.delete("/restaurant/main_menu/menu_category/:id", (request, response) => {
+  const { id, fk } = request.params
+  pool.query(
     'delete from gr.menu_category where menu_category_id=$1 && mainmenu_id=$2',
-    [id,fk],
+    [id, fk],
     (error) => {
       if (error) {
         throw error
@@ -204,12 +206,12 @@ app.use(bodyParser.urlencoded({ extended: true }))
       response.status(201).json({ status: 'success', message: 'menu category was deleted' })
     }
   )
-  })
-  app.put("/restaurant/main_menu/menu_category/",(request,response)=>{
-    const {id,name} = request.body
+})
+app.put("/restaurant/main_menu/menu_category/", (request, response) => {
+  const { id, name } = request.body
   pool.query(
     'UPDATE gr.menu_category set name=$2 where menu_category_id= $1',
-    [id,name],
+    [id, name],
     (error) => {
       if (error) {
         throw error
@@ -217,28 +219,28 @@ app.use(bodyParser.urlencoded({ extended: true }))
       response.status(201).json({ status: 'success', message: 'menu category update successfully' })
     }
   )
+})
+
+
+//----------------master menu------------------
+app.get("/restaurant/main_menu/menu_category/master_item/:id", (request, response) => {
+  // TODO:
+  const { id } = request.params;
+  pool.query('select * from gr.master_item where res_id=$1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
   })
 
+});
 
-  //----------------master menu------------------
-  app.get("/restaurant/main_menu/menu_category/master_item/:id", (request, response) => {
-    // TODO:
-    const { id } = request.params;
-    pool.query('select * from gr.master_item where res_id=$1',[id] ,(error, results) => {
-        if (error) {
-          throw error
-        }
-        response.status(200).json(results.rows)
-      })
-   
-  });
+app.post("/restaurant/main_menu/menu_category/master_item", (request, response) => {
+  const { id, name, price } = request.body;
 
-  app.post("/restaurant/main_menu/menu_category/master_item",(request,response)=>{
-    const {id,name,price } = request.body;
-    
-   pool.query(
+  pool.query(
     'INSERT INTO gr.master_item(res_id,item_name,price) VALUES ($1,$2,$3)',
-    [id,name,price],
+    [id, name, price],
     (error) => {
       if (error) {
         throw error
@@ -246,11 +248,11 @@ app.use(bodyParser.urlencoded({ extended: true }))
       response.status(201).json({ status: 'success', message: 'master item updated ' })
     }
   )
-  })
+})
 //TODO
-  app.delete("/restaurant/main_menu/menu_category/master_item/:id",(request,response)=>{
-    const {id} = request.params
-    pool.query(
+app.delete("/restaurant/main_menu/menu_category/master_item/:id", (request, response) => {
+  const { id } = request.params
+  pool.query(
     'delete from gr.master_item where menu_item_id=$1 ',
     [id],
     (error) => {
@@ -260,12 +262,12 @@ app.use(bodyParser.urlencoded({ extended: true }))
       response.status(201).json({ status: 'success', message: 'menu item was deleted' })
     }
   )
-  })
-  app.put("/restaurant/main_menu/menu_category/master_item",(request,response)=>{
-    const {id,price} = request.body
+})
+app.put("/restaurant/main_menu/menu_category/master_item", (request, response) => {
+  const { id, price } = request.body
   pool.query(
     'UPDATE gr.master_item set price=$2 where menu_item_id= $1',
-    [id,price],
+    [id, price],
     (error) => {
       if (error) {
         throw error
@@ -273,66 +275,129 @@ app.use(bodyParser.urlencoded({ extended: true }))
       response.status(201).json({ status: 'success', message: 'master item updated' })
     }
   )
-  })
-  //--------------menu_item----------------------------------
+})
+//--------------menu_item----------------------------------
 
-  app.get("/restaurant/main_menu/menu_category/master_item/menu_item/:id", (request, response) => {
-    // TODO:
-    const { id } = request.params;
-    pool.query('select res_name,item_name,price from gr.restaurant as a right join gr.main_menu as b on a.res_id=b.res_id right join gr.menu_category as c on b.mainmenu_id=c.mainmenu_id right join gr.menu_item as d on c.menu_category_id=d.menu_category_id right join gr.master_item as e on d.menu_item_id=e.menu_item_id where item_id=$1',[id] ,(error, results) => {
-        if (error) {
-          throw error
-        }
-        response.status(200).json(results.rows)
-      })
-   
-  });
+app.get("/restaurant/main_menu/menu_category/master_item/menu_item/:id", (request, response) => {
+  // TODO:
+  const { id } = request.params;
+  pool.query('select res_name,item_name,price from gr.restaurant as a right join gr.main_menu as b on a.res_id=b.res_id right join gr.menu_category as c on b.mainmenu_id=c.mainmenu_id right join gr.menu_item as d on c.menu_category_id=d.menu_category_id right join gr.master_item as e on d.menu_item_id=e.menu_item_id where item_id=$1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+
+});
 //-------------------------------------
-  // app.get("/restaurant/:id", (request, response) => {
-  //   // TODO:
-  //   const { id } = request.params;
-  //   pool.query('select res_name,item_name,price from gr.restaurant as a right join gr.main_menu as b on a.res_id=b.res_id right join gr.menu_category as c on b.mainmenu_id=c.mainmenu_id right join gr.menu_item as d on c.menu_category_id=d.menu_category_id right join gr.master_item as e on d.menu_item_id=e.menu_item_id where item_id=$1',[id] ,(error, results) => {
-  //       if (error) {
-  //         throw error
-  //       }
-  //       response.status(200).json(results.rows)
-  //     })
-   
-  // });
+// app.get("/restaurant/:id", (request, response) => {
+//   // TODO:
+//   const { id } = request.params;
+//   pool.query('select res_name,item_name,price from gr.restaurant as a right join gr.main_menu as b on a.res_id=b.res_id right join gr.menu_category as c on b.mainmenu_id=c.mainmenu_id right join gr.menu_item as d on c.menu_category_id=d.menu_category_id right join gr.master_item as e on d.menu_item_id=e.menu_item_id where item_id=$1',[id] ,(error, results) => {
+//       if (error) {
+//         throw error
+//       }
+//       response.status(200).json(results.rows)
+//     })
 
-  //------------------------------------
-  app.get("/restaurant/:id/q", (request, response) => {
-    // TODO:
-    const { id } = request.params;
-    const{ismenu}=request.query.ismenu;
-    console.log(ismenu);
-    pool.query('select * from gr.restaurant as a right join gr.main_menu as b on a.res_id=b.res_id right join gr.menu_category as c on b.mainmenu_id=c.mainmenu_id right join gr.menu_item as d on c.menu_category_id=d.menu_category_id right join gr.master_item as e on d.menu_item_id=e.menu_item_id where res_id=$1',[id] ,(error, results) => {
-        if (error) {
-          throw error
-        }
-        response.status(200).json(results.rows)
-      })
-      console.log("ismenu"+ismenu);
-      if(ismenu==true){
-        pool.query('select menu_name from gr.restaurant as a right join gr.main_menu as b on a.res_id=b.res_id right join gr.menu_category as c on b.mainmenu_id=c.mainmenu_id right join gr.menu_item as d on c.menu_category_id=d.menu_category_id right join gr.master_item as e on d.menu_item_id=e.menu_item_id where res_id=$1',[id] ,(error, results) => {
-          if (error) {
-            throw error
-          }
-          response.status(200).json(results.rows)
-        })
+// });
+
+//------------------------------------
+app.get("/restaurant1/:id", (request, response) => {
+  // TODO:
+  const { id } = request.params;
   
-      }
-   
-  });
+  const { ismenu } = request.query;
+  //console.log(ismenu);
+  let query = 'select * from gr.restaurant';
+  let querybinding = [];
+  //console.log(id+" "+ismenu)
+  if (ismenu === 'true') {
+    query = `select  res_name,menu_name 
+    from gr.restaurant as a 
+    inner join gr.main_menu as b on a.res_id=b.res_id
+    where b.res_id=$1;`;
+    querybinding=[id];
+    //console.log(id)
+  }
   
-  
-
-
-
-
-
-
-
-  app.listen(process.env.PORT || 3000, () => {
-    console.log(`Server listening`)
+  pool.query(query,querybinding, (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
   })
+});
+
+app.get("/restaurant1/:id/:cat", (request, response) => {
+  // TODO:
+  const { id,cat} = request.params;
+  const { iscat } = request.query;
+  //console.log(ismenu);
+  let query = `select  res_name,menu_name 
+  from gr.restaurant as a 
+  inner join gr.main_menu as b 
+  on a.res_id=b.res_id where b.res_id= $1;`;
+  let querybinding = [id];
+  //console.log(id+" "+ismenu)
+  if (iscat === 'true') {
+    query = `select res_name,menu_name,name 
+    from gr.restaurant as a 
+    inner join gr.main_menu as b on a.res_id=b.res_id 
+    inner join gr.menu_category as c on b.mainmenu_id=c.mainmenu_id 
+    where a.res_id =$1 and b.mainmenu_id = $2;`;
+    querybinding=[id,cat];
+    console.log(id)
+  }
+  
+  pool.query(query,querybinding, (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+  
+});
+///==============================
+app.get("/restaurant1/:id/:cat/:menu", (request, response) => {
+  // TODO:
+  const { id,cat,menu } = request.params;
+  const { ismenu } = request.query;
+  //console.log(ismenu);
+  let query = `select 
+  res_name,menu_name,name as catname 
+  from gr.restaurant as a 
+  inner join gr.main_menu as b on a.res_id=b.res_id 
+  inner join gr.menu_category as c on b.mainmenu_id=c.mainmenu_id 
+  where a.res_id =$1 
+  and b.mainmenu_id = $2;`;
+  let querybinding = [id,cat];
+  //console.log(id+" "+ismenu)
+  if (ismenu === 'true') {
+    query = `select 
+    res_name,menu_name,name,item_name,price 
+    from gr.restaurant as a 
+     join gr.main_menu as b on a.res_id=b.res_id 
+     join gr.menu_category as c on b.mainmenu_id=c.mainmenu_id 
+     join gr.menu_item as d on c.menu_category_id=d.menu_category_id 
+     join gr.master_item as e on d.menu_item_id=e.menu_item_id 
+    where 
+    a.res_id=$1 
+    and b.mainmenu_id=$2 
+    and c.menu_category_id =$3;`;
+    querybinding=[id,cat,menu];
+    console.log(id)
+  }
+  
+  pool.query(query,querybinding, (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+  
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server listening`)
+})
